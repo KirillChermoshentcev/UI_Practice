4788fdb925d05ca6921a83f04b0921dcc97851ff
KirillChermoshentcev <kirill.chermoshentcev@yandex.ru>
2025-02-09 17:13:45 +0300

    first action

--- conftest.py
+++ conftest.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(scope='function', autouse=True)
 def driver(request):
     options = Options()
+    options.add_argument('--headless')
     driver = webdriver.Chrome(options=options)
     driver.get('https://demoqa.com/elements')
     driver.maximize_window()
